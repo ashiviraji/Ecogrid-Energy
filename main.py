534d6b8c040f1a99696a2e1392551e4f1757f3e8
Shirmila Sewwandi <105427132+Shirmila-Sewwandi@users.noreply.github.com>
2026-05-06 01:23:49 +1000

    updated main file to test the code

---
+++ main.py
@@ -0,0 +1,12 @@
+
+#for meter service------------
+
+from meter_service import MeterService
+
+# Create MeterService object
+meter = MeterService()
+
+# Run processing function
+meter.process_meter_readings()
+
+#--------meter service---------
